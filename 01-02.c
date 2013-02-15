0c2f8bad6c6f2881d0d59599711a523c45d327c6
Jiri Chara <jirik.chara@gmail.com>
2013-02-15 15:31:16 +0100

    Solved excersices: 02 - 05

---
+++ 01-02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Test \\a\a\n");
+	printf("Test \\c\c\n");
+	printf("Test \\$\$\n");
+	printf("Test \\v\v\n");
+	printf("Test \\f\f\n");
+	printf("Test \\r\r\n");
+
+	return 0;
+}
